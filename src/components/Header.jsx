8e9c9e456b7626fd5f08f5d5ef8d9a186edb6cfd
sharmaaaditi <sharma.aaditi10@gmail.com>
2026-05-30 23:32:48 +0530

    Create homepage header and hero section

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+
+function Header(){
+    return(
+        <div>
+            <div className="header">
+                <h2>CleanTrack</h2>
+                
+                <nav>
+                    <ul className="nav_link">
+                       
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Report Issue</a></li>
+                        <li><a href="#"> Dashboard</a></li>
+                        <li><a href="#"> About</a></li>
+                    </ul>
+            
+                </nav>
+        </div>
+        </div>
+    )
+}
+export default Header;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero(){
+    return(
+        <div className="hero">
+            <h1>Report Waste. Track Cleanliness. Build a Cleaner Campus.</h1>
+            <p>CleanTrack helps students report waste and pollution issues quickly,
+        so the campus stays clean and well managed.</p>
+            <button>Report Issue</button>
+        </div>
+    )
+}
+export default Hero;
