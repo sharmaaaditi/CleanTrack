bb9276deb66fa1b7a2529a5737a3e8004acd0b30
sharmaaaditi <sharma.aaditi10@gmail.com>
2026-05-31 13:53:40 +0530

    Add hero background and animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,15 @@
+import React, { useState } from "react";
 function Hero(){
+    const [move, setMove] = useState(false);
     return(
-        <div className="hero">
+        <section className="hero" onClick={() => setMove(!move)}>
+        <div className={`hero_content ${move ? "circleMove" : ""}`}>
             <h1>Report Waste. Track Cleanliness. Build a Cleaner Campus.</h1>
             <p>CleanTrack helps students report waste and pollution issues quickly,
         so the campus stays clean and well managed.</p>
             <button>Report Issue</button>
         </div>
+        </section>
     )
 }
 export default Hero;
