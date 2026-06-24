d27eee98aa746698a38b0cb573b69880b34cc87a
sharmaaaditi <sharma.aaditi10@gmail.com>
2026-06-24 23:06:07 +0530

    Refactor project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import Header from "./components/header";
-import Hero from "./components/Hero";
+
 function App(){
     return(
         <div>
-            <Header/>
-            <Hero/>
+            
         </div>
     )
 }
