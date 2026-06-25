d514f8a44d2a9dd5f7d73ed1f79a34c38d567605
chiraggautam0302 <chiraggautam0302@gmail.com>
2026-06-26 00:51:08 +0530

    Build project structure and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-function App(){
-    return(
-        <div>
-            
-        </div>
-    )
+import Home from "./pages/Home";
+import Report from "./pages/Report";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
 export default App;
